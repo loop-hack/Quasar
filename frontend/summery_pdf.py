185741a2c8744829f5ecb171dc6729c90793da56
loop-hack <timetraveller1732@gmail.com>
2026-03-23 01:26:40 +0530

    testing combined code of data processing and summery generation

--- frontend/summery_pdf.py
+++ frontend/summery_pdf.py
@@ -1,5 +1,8 @@
 from transformers import AutoModel
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.pagesizes import A4
 
 
 model = AutoModel.from_pretrained("bert-base-cased")
@@ -60,3 +63,17 @@ for i, chunk in enumerate(chunks):
 
     print(f"Summary {i+1} done.\n")
     final_summary += chunk_summary + "\n\n"
+
+
+doc = SimpleDocTemplate("summary_report.pdf", pagesize=A4)
+
+styles = getSampleStyleSheet()
+style = styles["Normal"]
+story = []
+
+story.append(Paragraph("<b>Document Summary</b>", styles['Title']))
+story.append(Paragraph(final_summary.replace("\n", "<br/>"), style))
+
+doc.build(story)
+
+print("PDF saved as summary_report.pdf")
