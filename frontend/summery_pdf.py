c1d4b11267d0e867acffd63fb0f82e1f3e342d69
loop-hack <timetraveller1732@gmail.com>
2026-03-23 01:15:26 +0530

    loaded model and tokenzer code used

--- frontend/summery_pdf.py
+++ frontend/summery_pdf.py
@@ -1,3 +1,14 @@
 from transformers import AutoModel
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
 
 model = AutoModel.from_pretrained("bert-base-cased")
+
+
+save_path = "./bart-large-cnn"
+
+tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
+model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn")
+
+tokenizer.save_pretrained(save_path)
+model.save_pretrained(save_path)
